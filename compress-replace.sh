9ccdb9d6bec7db8dc01cd22b1223f40c1b7f23d1
wszqkzqk <wszqkzqk@qq.com>
2022-01-10 16:12:14 +0800

    Updated by update.sh

---
+++ compress-replace.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+cd `dirname $0`; pwd
+for i in *.pdf ; do
+    if [[ $i != *-pic* ]] ; then
+        echo "正在压缩文件：$i"
+        gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dQUIET -dCompatibilityLevel=1.4 -sOutputFile=${i:0:-4}-compressed.pdf $i
+        mv ${i:0:-4}-compressed.pdf $i
+        echo "已完成！"
+    fi
+done
