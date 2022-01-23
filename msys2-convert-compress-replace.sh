b64c7a70c6f59e879a5eb780ebcf10a44803f108
星外之神 <wszqkzqk@qq.com>
2022-01-23 11:53:53 +0800

    u

---
+++ msys2-convert-compress-replace.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+cd `dirname $0`; pwd
+for i in *.pdf ; do
+    if [[ $i != *-pic-compressed* ]] ; then
+        echo "正在转化文件：$i"
+        pdftoppm -png $i ${i:0:-4}
+        gm.exe convert ${i:0:-4}*.png $i
+        rm ${i:0:-4}*.png
+        gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dQUIET -dCompatibilityLevel=1.4 -sOutputFile=${i:0:-4}-compressed.pdf $i
+        mv ${i:0:-4}-compressed.pdf $i
+        echo "已完成！"
+    fi
+done
---
+++ msys2-convert.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+cd `dirname $0`; pwd
+for i in *.pdf ; do
+    if [[ $i != *-pic* ]] ; then
+        echo "正在转化文件：$i"
+        pdftoppm -png $i ${i:0:-4}
+        gm.exe convert ${i:0:-4}*.png ${i:0:-4}-pic.pdf
+        rm ${i:0:-4}*.png
+        echo "已输出至：${i:0:-4}-pic.pdf"
+    fi
+done
